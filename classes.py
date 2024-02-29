3bde36214c83342d0d8297795a1780e570aadae4
brynbarker <bryn.nicole.barker@gmail.com>
2024-02-29 00:00:01 -0500

    29Feb2024

--- classes.py
+++ classes.py
@@ -2,19 +2,120 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Node:
-    def __init__(self,j,i):
+    def __init__(self,j,i,x,y,h):
+        self.ID
         self.j = j
         self.i = i
+        self.x
+        self.y
+        self.h
 
 class Element:
-    def __init__(self,j,i):
+    def __init__(self,ID,j,i,x,y,h):
+        self.ID = ID
         self.j = j
         self.i = i
-        self.nodes = []
+        self.x = x
+        self.y = y
+        self.h = h
+        self.dof_ids = []
+        self.dof_list = []
         self.stiffness = None
 
+    def add_dofs(self,strt,xlen):
+        if len(self.dof_ids) != 0:
+            return
+        for ii in range(4):
+            for jj in range(4):
+                self.dof_ids.append(strt+xlen*ii+jj)
+        return
+
+    def update_dofs(self,dofs):
+        if len(self.dof_list) != 0:
+            return
+        for dof_id in self.dof_ids:
+            dof = dofs[dof_id]
+            self.dof_list.append(dof)
+        return
+
 class Mesh:
-    def __init(self,N,xlim=[0,1],ylim=[0,1]):
+    def __init(self,N):
+        self.N = N # number of fine elements 
+                   # from x=0.5 to x=1.0
+        self.h = 0.5/N
+        self.dofs = {}
+        self.elements = {}
+        self.boundaries = []
+        self.interface = [[],[]]
+        
+        self._make_coarse()
+        self._make_fine()
+
+        self._update_elements()
+
+    def _make_coarse(self):
+        H = self.h*2
+        xdom = np.linspace(0-H,0.5+H,int(self.N/2)+3)
+        ydom = np.linspace(0-H,1+H,self.N+3)
+
+        xlen,ylen = len(xdom),len(ydom)
+
+        dof_id,e_id = 0,0
+        for i,y in enumerate(ydom):
+            for j,x in enumerate(xdom):
+                self.dofs[dof_id] = Node(j,i,x,y,H)
+
+                if (0<=x<.5) and (0<=y<1.):
+                    strt = dof_id-1-xlen
+                    element = Element(e_id,j-1,i-1,x,y,H)
+                    element.add_dofs(strt,xlen)
+                    self.elements[e_id] = element
+                    e_id += 1
+
+                if x==0. or y==0. or y==1:
+                    if (0<=x<=.5) and (0<=y<=1.):
+                        self.boundaries.append(dof_id)
+                elif x==0.5 and 0<y<1:
+                    self.interface[0].append(dof_id)
+
+                dof_id += 1
+
+        self.n_coarse_dofs = dof_id
+        self.n_coarse_els = e_id
+
+    def _make_fine(self):
+        H = self.h
+        xdom = np.linspace(0.5-H,1.+H,self.N+3)
+        ydom = np.linspace(0-H,1+H,2*self.N+3)
+
+        dof_id,e_id = self.n_coarse_dofs,self.n_coarse_els
+        for i,y in enumerate(ydom):
+            for j,x in enumerate(xdom):
+                self.dofs[dof_id] = Node(j,i,x,y,H)
+
+                if (0.5<=x<1.) and (0<=y<1.):
+                    strt = dof_id-1-xlen
+                    element = Element(e_id,j-1,i-1,x,y,H)
+                    element.add_dofs(strt,xlen)
+                    self.elements[e_id] = element
+                    e_id += 1
+
+                if x==1. or y==0. or y==1:
+                    if (.5<=x<=.1) and (0<=y<=1.):
+                        self.boundaries.append(dof_id)
+                elif x==0.5 and 0<y<1:
+                    self.interface[1].append(dof_id)
+
+                dof_id += 1
+
+    def _update_elements(self):
+        for e in self.elements:
+            e.update_dofs(self.dofs)
+
+class Laplace:
+    def __init(self,N,u,f):
         self.N = N
-        self.xlim = xlim
-        self.ylim = ylim
+        self.ufunc = u
+        self.ffunc = f
+
+        self.mesh = Mesh(N)
---
+++ shape_functions.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def phi3(x,h):
+    if -2*h < x <= -h:
+        return (x+3*h)*(x+2*h)*(x+h)/6/h**3
+    elif -h < x <= 0:
+        return -(x+2*h)*(x+h)*(x-h)/2/h**3
+    elif 0 < x <= h:
+        return (x+h)*(x-h)*(x-2*h)/2/h**3
+    elif h < x <= 2*h:
+        return -(x-h)*(x-2*h)*(x-3*h)/6/h**3
+    else:
+        return 0
+
+def phi3_interface(y,h,s,coll=True):
+    end_val = 1-11/3*s+4*s**2-4/3*s**3
+    if coll:
+        scale = abs(y)/2/h
+        return phi3(s*y,h)+scale*end_val
+    if -2*h < y <= -h:
+        return phi3(s*(y-h)+h,h)
+    elif -h < y <= 0:
+        return 1-phi3(s*(y-h),h)-phi3(s*(y+h),h)-phi3(s*(y+h)+h)+y*end_val
+    elif 0 < y <= h:
+        return 1-phi3(s*(y-h),h)-phi3(s*(y+h),h)-phi3(s*(y-h)-h)-y*end_val
+    elif h < y <= 2*h:
+        return phi3(s*(y+h)-h,h)
+    else:
+        return 0
+
+def phi3_interface_dy(y,h,s,coll=True):
+    end_val = 1-11/3*s+4*s**2-4/3*s**3
+    if coll:
+        sgn = np.sign(y)
+        return s*phi3_dx(s*y,h)+sgn*end_val/2/h
+    if -2*h < y <= -h:
+        return s*phi3_dx(s*(y-h)+h,h)
+    elif -h < y <= 0:
+        return -s*phi3_dx(s*(y-h),h)-s*phi3_dx(s*(y+h),h)\
+               -s*phi3_dx(s*(y+h)+h)+end_val
+    elif 0 < y <= h:
+        return -s*phi3_dx(s*(y-h),h)-s*phi3_dx(s*(y+h),h)\
+               -s*phi3_dx(s*(y-h)-h)-end_val
+    elif h < y <= 2*h:
+        return s*phi3_dx(s*(y+h)-h,h)
+    else:
+        return 0
+
+def phi3_interface_ds(y,h,s,coll=True):
+    end_val = -11/3+8*s-4*s**2
+    if coll:
+        scale = abs(y)/2/h
+        return y*phi3_dx(s*y,h)+scale*end_val
+    if -2*h < y <= -h:
+        return (y-h)*phi3_dx(s*(y-h)+h,h)
+    elif -h < y <= 0:
+        return -(y-h)*phi3_dx(s*(y-h),h)-(y+h)*phi3_dx(s*(y+h),h)\
+               -(y+h)*phi3_dx(s*(y+h)+h)+y*end_val
+    elif 0 < y <= h:
+        return -(y-h)*phi3_dx(s*(y-h),h)-(y+h)*phi3_dx(s*(y+h),h)\
+               -(y-h)*phi3_dx(s*(y-h)-h)-y*end_val
+    elif h < y <= 2*h:
+        return (y+h)*phi3_dx(s*(y+h)-h,h)
+    else:
+        return 0
+
+def phi3_interface_dx(y,h,s,coll=True):
+    return phi3_interface_ds(y,h,s,coll)/2/h
+
+def phi3_interface_eval(y_in,h,y0,s):
+    y = y_in-y0
+    coll = int(y0/h)%2==0
+    return phi3_interface(y,h,s,coll)
+     
+
+def phi3_dx(x,h):
+    if -2*h < x <= -h:
+        return (11*h**2+12*h*x+3*x**2)/6/h**3
+    elif -h < x <= 0:
+        return (h**2-4*h*x-3*x**2)/2/h**3
+    elif 0 < x <= h:
+        return -(h**2+4*h*x-3*x**2)/2/h**3
+    elif h < x <= 2*h:
+        return -(11*h**2-12*h*x+3*x**2)/6/h**3
+    else:
+        return 0
+
+def phi3_2d(x,y,h):
+    return phi3(x,h)*phi3(y,h)
+
+def grad_phi3(x,y,h,s=None,interface=False,coll=True):
+    phi_j = phi3(x,h)
+    dphi_j_dx = phi3_dx(x,h)
+    if interface:
+        phi_i = phi3_interface(y,h,s,coll)
+
+        dphi_i_dx = phi3_interface_dx(y,h,s,coll)
+
+        dphi_i_dy = phi3_interface_dy(y,h,s,coll)
+        return np.array([phi_j*dphi_i_dx+phi_i*dphi_j_dx,phi_j*dphi_i_dy])
+    phi_i = phi3(y,h)
+    dphi_i_dy = phi3_dx(y,h)
+    return np.array([phi_i*dphi_j_dx,phi_j*dphi_i_dy])
+
+def grad_phi3_eval(x_in,y_in,h,x0,y0):
+
+
+def phi3_2d_eval(x_in,y_in,x0,y0,h):
+    x,y = x_in-x0, y_in-y0
+    dist = x_in-0.5
+
+    if 0 < dist <= h:
+        s
+        interface = True
+        collocated = y0/h
+    return phi3_2d(x,y,h)
+
+
