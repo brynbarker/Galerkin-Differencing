f24e344749c653a183b29c7b3a7257b405da4b6e
brynbarker <bryn.nicole.barker@gmail.com>
2025-03-08 15:41:50 -0500

    3d lin horiz working

---
+++ tests/linear_basis/horizontal_mass_convergence.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from linear_basis.mac_grid.horiz_classes import HorizontalRefineSolver
+import numpy as np
+
+output = []
+
+u = lambda x,y,z: np.sin(2*np.pi*x)*np.cos(2*np.pi*y)*np.sin(2*np.pi*z)
+
+# set up solvers
+prev = 1
+for N in [8,16]:
+	href = HorizontalRefineSolver(N,u)
+	href.projection()
+
+	err = href.error(proj=True)
+	if prev != 1:
+		output.append(prev/4 > err)
+	prev = err
+
+### write results to output file
+with open("output", 'w') as handle:
+    for o in output:
+        handle.write(str(o) + "\n")
+
